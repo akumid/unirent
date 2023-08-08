fa7fbecb1ceee79e7f4cc23bb082d9d4a6a812cd
cheechee97 <45931050+cheechee97@users.noreply.github.com>
2023-08-09 00:00:02 +0800

    api key

--- src/services/LocationSearch.tsx
+++ src/services/LocationSearch.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const LocationSearch = async (query: String) => {
     // will implement the api key in the future
-    const key = "AIzaSyDZT_MBguU9mKb0G-6Kffv7PKFy94X9Adk";
+    const key = "test";
     const url = "https://maps.googleapis.com/maps/api/place/queryautocomplete/json?input=" + query + "&key=" + key;
 
     const response = await axios.get(url).then((locations) => {
