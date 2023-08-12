fd4ef5fe494f54d46ff3b63f97b2dc8a7781d4a9
akumid <tommysjafii@gmail.com>
2023-08-12 15:08:31 +0800

    configure and init amplify

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { Amplify } from "aws-amplify";
 import { StatusBar } from "expo-status-bar";
 import { PaperProvider } from "react-native-paper";
 
-import AuthStack from "./src/navigation/AuthStack"; // TODO: to be replace with AWS Cognito
+import awsExports from "./src/aws-exports";
+import AuthStack from "./src/navigation/AuthStack";
+
+Amplify.configure(awsExports); // TODO: to be replace with AWS Cognito
 
 export default function App() {
   return (
