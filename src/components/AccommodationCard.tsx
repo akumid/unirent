1e95db42056535e2a5b2bf44c94d85b030586e85
CHEE LIM PENG <45931050+jaydenchee97@users.noreply.github.com>
2023-08-19 15:13:26 +0800

    search result screen implementation and accommodation card component
    accommodation card and search result screen implementation

---
+++ src/components/AccommodationCard.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Dimensions, ScrollView, Image, TouchableWithoutFeedback } from "react-native";
+import { Searchbar, Card, Button, Text, Divider, Avatar } from "react-native-paper";
+import { SliderBox } from "react-native-image-slider-box";
+import Feather from 'react-native-vector-icons/Feather';
+
+const AccommodationCard = (props: any) => {
+
+    const images = [
+        'https://media.karousell.com/media/photos/products/2019/07/02/master_room_for_rent_at_clementi_1562052953_90c3c04e0_progressive',
+        'https://cdn-cms.pgimgs.com/static/2021/06/958-Hougang-Street-91-Hougang-Punggol-Sengkang-Singapore.jpg',
+        'https://media.karousell.com/media/photos/products/2020/7/28/shunfu_road_hdb_room_rental_1595903271_2f1e723b_progressive'
+    ]
+
+    return (
+        <Card 
+        style={{
+            flex: 1, 
+            backgroundColor: '#F4EDF9', 
+            borderColor: '#10161F', 
+            borderWidth: 0.3, 
+            marginVertical: 15
+        }} 
+        onPress={() => console.warn("navigate to room detail screen")}
+        >
+        <SliderBox style={{height: 250}}  images={images} />
+
+        <View style={{flexDirection : 'row'}}>
+            <Card.Title title="Clementi Condominium" subtitle="441B Clementi Avenue" subtitleNumberOfLines={3} subtitleVariant="labelMedium" subtitleStyle={{color: 'gray'}} style={{flex: 1}}/>
+            <Button mode="outlined" style={{zIndex: 1, justifyContent: 'center', alignItems: 'center', height: 50, width: 'auto', marginVertical: 20, marginHorizontal: 20,}} onPress={() => console.warn("Save accommodation")}> 
+                <Feather name='heart' size={20} /> 
+            </Button>
+        </View>
+        <Divider />
+        <Card.Content style={{ marginVertical: 10}}>
+            <Text>$1000 / month • Available from 25 Aug  </Text>
+        </Card.Content>
+        <Divider />
+        <Card.Content style={{marginVertical: 10}}>
+            <View style={{flexDirection : 'row', marginBottom: 10}}>
+                <Avatar.Text size={50} label="User" />
+                <View style={{justifyContent: 'center', marginHorizontal: 10, flexDirection: 'column'}}>
+                    <Text style={{fontSize: 12, fontWeight: 'bold'}}> Listed By User 1 </Text>
+                    <Text style={{fontSize: 14,}}> short description of the room </Text>
+                </View>
+            </View>
+            <Button mode="outlined" onPress={() => console.warn("navigate to message screen")}> Contact </Button>
+
+        </Card.Content>
+
+      </Card>
+    );
+
+}
+
+export default AccommodationCard;
--- src/screens/SearchResultScreen.tsx
+++ src/screens/SearchResultScreen.tsx
@@ -1,34 +1,24 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Dimensions, ScrollView, Image } from "react-native";
+import { View, StyleSheet, Dimensions, ScrollView, Image, TouchableWithoutFeedback } from "react-native";
 import { Searchbar, Card, Button, Text, Divider, Avatar } from "react-native-paper";
-import Swiper from 'react-native-swiper'
-import Feather from 'react-native-vector-icons/Feather';
+import AccommodationCard from "../components/AccommodationCard";
 
 
 const SearchResultScreen = (props: any) => {
 
     const [search, setSearch] = useState('');
-    const images = [
-        'https://media.karousell.com/media/photos/products/2019/07/02/master_room_for_rent_at_clementi_1562052953_90c3c04e0_progressive',
-        'https://media.karousell.com/media/photos/products/2019/07/02/master_room_for_rent_at_clementi_1562052953_90c3c04e0_progressive',
-        'https://media.karousell.com/media/photos/products/2019/07/02/master_room_for_rent_at_clementi_1562052953_90c3c04e0_progressive'
-    ]
-
-    const subtitle = () => {
-        return (
-            <View style={{flexDirection: 'column'}}>
-                <Text>
-                    441B Clementi Avenue 3
-                </Text>
-                <Text>
-                    subtitle 1
-                </Text>
-            </View>
-        );
-    }
 
     return (
-    <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-start', top: '7%', marginHorizontal: 20}}>
+    <View style={{
+        flex: 1, 
+        flexDirection: 'column', 
+        justifyContent: 'flex-start', 
+        top: '7%', 
+        marginHorizontal: 20, 
+        shadowRadius: 3, 
+        shadowOpacity:  0.4, 
+        shadowOffset: {width: 1, height: 1}
+    }}>
         <View style={{alignItems: 'center'}}>
           <Searchbar 
             placeholder="Search Location"
@@ -37,49 +27,17 @@ const SearchResultScreen = (props: any) => {
               setSearch(query); 
             }}
             value={search}
-            style={{ width: '95%', marginBottom: 50, }}
+            style={{ width: '95%', marginBottom: 30, }}
           />
+          <Divider />
         </View>
 
   
         <ScrollView>
-          <Card style={{flex: 1, backgroundColor: '#F4EDF9', borderColor: '#10161F', borderWidth: 0.3}} >
-            <Swiper style={{height: 250, marginTop: 5}} >
-                <View>
-                    <Image source={{uri: images[0]}} style={{width: '100%', height: '100%'}} />
-                </View>
-                <View>
-                    <Image source={{uri: images[0]}} style={{width: '100%', height: '100%'}} />
-                </View>
-                <View>
-                    <Image source={{uri: images[0]}} style={{width: '100%', height: '100%'}} />
-                </View>
-            </Swiper>
-
-            <View style={{flexDirection : 'row'}}>
-                <Card.Title title="Clementi Condominium" subtitle="441B Clementi Avenue" subtitleNumberOfLines={3} subtitleVariant="labelMedium" subtitleStyle={{color: 'gray'}} style={{flex: 1}}/>
-                <Button mode="outlined" style={{justifyContent: 'center', alignItems: 'center', height: 50, width: 'auto', marginVertical: 20, marginHorizontal: 20,}} onPress={() => console.warn("Save accommodation")}> 
-                    <Feather name='heart' size={20} /> 
-                </Button>
-            </View>
-            <Divider />
-            <Card.Content style={{ marginVertical: 10}}>
-                <Text>$1000 / month • Available from 25 Aug  </Text>
-            </Card.Content>
-            <Divider />
-            <Card.Content style={{marginVertical: 10}}>
-                <View style={{flexDirection : 'row', marginBottom: 10}}>
-                    <Avatar.Text size={50} label="User" />
-                    <View style={{justifyContent: 'center', marginHorizontal: 10, flexDirection: 'column'}}>
-                        <Text style={{fontSize: 12, fontWeight: 'bold'}}> Listed By User 1 </Text>
-                        <Text style={{fontSize: 14,}}> short description of the room </Text>
-                    </View>
-                </View>
-                <Button mode="outlined" onPress={() => console.warn("navigate to message screen")}> Contact </Button>
-
-            </Card.Content>
-
-          </Card>
+          <AccommodationCard />
+          <AccommodationCard />
+          <AccommodationCard />
+          <AccommodationCard />
         </ScrollView>
     </View>
     )
